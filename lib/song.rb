4423ddfa52bd0c25c8a7b3297475997d97cc1d85
ktochow1 <ktochow1@binghamton.edu>
2018-11-04 21:46:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,19 @@ class Song
       end
     end
   end
+  
+   def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      artist
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      # exit!
+      end
+    end
+  end
 
 
 end
