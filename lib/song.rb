48f4187691982085190be2ce54ee137b24956d0d
ktochow1 <ktochow1@binghamton.edu>
2018-11-04 21:43:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      genre
+      genre.count
       if genre_count[genre]
         genre_count[genre] += 1
       else
